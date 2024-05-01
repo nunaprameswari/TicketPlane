c078c4c6ef133d00cddeafe8b831e1e06391f42f
nindiappc <nindi.nindia44@gmail.com>
2024-05-01 10:35:27 +0700

    import di app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Beranda from './screens/Beranda';
+import Tiket from './screens/Tiket';
+import Akun from './screens/Akun';
 
 const App = () => {
   return (
